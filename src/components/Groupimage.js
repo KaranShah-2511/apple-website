06e1039c3d8336e30087c40e7e6a4d6766435e0a
KaranShah-2511 <karansshah25@gmail.com>
2021-07-14 19:44:41 +0530

    for live

--- src/components/Groupimage.js
+++ src/components/Groupimage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
-
 function Groupimage({
   lefttitle,
   righttitle,
